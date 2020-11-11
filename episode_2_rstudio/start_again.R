b23f8f6da5b09fd5825d8d605d2eedeba1728d5c
Allan Lui <allanlui@hotmail.com>
2020-11-11 13:51:42 +0000

    Initial commit

---
+++ episode_2_rstudio/start_again.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+
+setwd("C:/Users/Allan Lui/Documents/roar/episode_2_rstudio")
+
+set.seed(10)
+x <- rnorm(10)
+
+sum <- 0
+
+for(i in 1:length(x)){
+  sum <- sum + x[[i]]
+}
+
+print(sum)
+# save.image() and savehistory() are only needed for this lesson in RStudio cloud for 
+# demonstration purposes - don't put them in your scripts!
+save.image()
+savehistory()
+
+z<- runif(n = 10, min = 1, max = 2)
+
+# I love R
+# I love R
+# I love R
+# I love R
+# I love R
+
+
